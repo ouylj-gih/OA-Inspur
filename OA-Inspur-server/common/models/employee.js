5e16978e42a272959921e273904dd6c81c8a22b9
louyang <824808420@qq.com>
2019-12-23 17:18:26 +0800

    modify

--- OA-Inspur-server/common/models/employee.js
+++ OA-Inspur-server/common/models/employee.js
@@ -6,57 +6,90 @@ const redis = require("../../server/lib/datasources/redis");
 const randomString = require("randomstring");
 
 module.exports = function (Employee) {
-  function defineGetContacts(app, cb) {
-
+  function defineGetContactsModel(app, cb) {
     const getContactsModel = {
-      "pageSize": {
-        "type": "number",
-        "required": true,
-        "description": "每页数量"
+      pageSize: {
+        type: 'number',
+        required: true,
+        description: "每页数量"
       },
-      "pageNumber": {
-        "type": "number",
-        "required": true,
-        "description": "当前页码"
+      pageNumber: {
+        type: 'number',
+        required: true,
+        description: "当前页码 "
       }
-    }
+    };
+
     var ds = app.datasources.db;
     ds.define('getContactsModel', getContactsModel, {
       idInjection: false
     });
+    cb(null, app);
+  }
 
-    Employee.getContacts = function (msg, cb) {
-      console.log(msg)
-      Employee.find({
-        where: {
-          display: 1
-        },
-        limit: Number(msg.pageSize),
-        offset: msg.pageNumber * msg.pageSize,
-        include: ["orgnization", "position"]
-      }, function (err, res) {
-        if (err) {
-          return cb(utils.clientError(err, 400), null)
+  function defineGetContacts(app, cb) {
+    Employee.getContacts = function (info, cb) {
+      function checkModelValid(cb) {
+        const Model = app.datasources.db.getModel('getContactsModel');
+        var model = new Model(info);
+        model.isValid(function (valid) {
+          if (!valid) {
+            cb(utils.clientError(model.errors), null);
+          } else {
+            cb(null, true);
+          }
+        });
+      }
+
+      function getContacts(ign, cb) {
+        if (!info.pageSize) {
+          info.pageSize = 10;
         }
-        cb(null, res)
+        if (!info.pageNumber) {
+          info.pageNumber = 0;
+        }
+        Employee.find({
+          where: {
+            display: 1
+          },
+          order: 'id desc',
+          limit: Number(info.pageSize),
+          offset: info.pageNumber * info.pageSize,
+          include: ["orgnization", "position"]
+        }, (err, logList) => {
+          if (err) {
+            return cb(utils.clientError('查询通讯录错误: ' + err), null);
+          }
+          cb(null, logList);
+        })
+      }
+
+      _async.waterfall([
+        checkModelValid,
+        getContacts
+      ], function (err, result) {
+        if (err) {
+          return cb(err, null);
+        }
+        cb(null, result)
       })
     }
 
     Employee.remoteMethod('getContacts', {
       http: {
-        verb: 'GET',
+        verb: 'GET'
       },
-      description: "获取通讯录",
+      description: '获取通讯录',
       accepts: {
-        type: "getContactsModel",
-        arg: "info",
+        arg: 'info',
+        type: 'getContactsModel',
         http: {
           source: 'query'
         }
       },
       returns: {
         arg: 'result',
-        type: 'array',
+        type: 'Employee',
         root: true
       }
     });
@@ -64,6 +97,7 @@ module.exports = function (Employee) {
 
   _async.waterfall([
     Employee.getApp.bind(Employee),
+    defineGetContactsModel,
     defineGetContacts
   ])
-}
+};
