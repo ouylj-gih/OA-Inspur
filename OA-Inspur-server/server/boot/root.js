443600997cdcf4b7f18237fab2787af18ac31ada
huntxiao <741929222@qq.com>
2020-01-02 16:55:54 +0800

    add loopback storage for portraits

--- OA-Inspur-server/server/boot/root.js
+++ OA-Inspur-server/server/boot/root.js
@@ -5,9 +5,14 @@
 
 'use strict';
 
+const path = require('path');
+
 module.exports = function (server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  router.get('/avatar', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../storage/defaultUser.jpg'));
+  })
   server.use(router);
 };
